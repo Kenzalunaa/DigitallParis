a4d7b16302aab1b8b1211abdb922aa0703928a65
dauwemarc <56584311+dauwemarc@users.noreply.github.com>
2020-11-26 10:13:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Accueil</title>
+
+    <link rel="stylesheet" href="css/main.css">
+
+  </head>
+  <body>
+    <div class="block">
+      <header class="header">
+        <a href="#" class="header-logo">Test</a>
+        <nav class="header-menu">
+          <a href="index.php">Accueil</a>
+          <a href="view/connexion.php">Connexion</a>
+          <a href="view/inscription.php">Inscription</a>
+        </nav>
+      </header>
+    </div>
+
+    <div class="block">
+      <div class="banner">
+        <img src="img/voituresgarees.jpg" class="banner-image">
+        <div class="banner-content">
+          <h1 class="title is-1">Test - Accueil</h1>
+          <h2 class="subtitle">DigitallParis <br> L’antivol automobile universel</h2>
+          <br><br><br><br><br>
+
+              <h1>Lockness</h1>
+            <h2>- Antivol automobile</h2>
+            <h2>- Installation</h2>
+            <h2>- Application mobile</h2>
+            <h2>- Autocollant</h2>
+            <h2>- Maintenance et mises à jour</h2>
+          <a href="view/connexion.php" class="btn btn-primary btn-lg" role="button">S'abonner</a>
+        </div>
+      </div>
+    </div>
+
+        <div class="percent">
+          <p>
+            <span class="percent-value">140200</span>
+            <span class="percent-sign"></span>
+          </p>
+        </div>
+        <h3 class="title">voitures volées en 2019 en France
+      </h3>
+
+      <div class="pb_text">
+        <h3>
+          <span>
+            "1 voiture volée toutes les "
+            <span style="color: #bc9f3b;">4minutes</span>
+          </span>
+        </h3>
+
+    <script src="js/main.js"></script>
+  </body>
+
+</html>
