d57976813ed56a770aa9324b2731434855f80e86
dauwemarc <56584311+dauwemarc@users.noreply.github.com>
2020-11-26 10:13:13 +0100

    Delete deconnexion.php

--- deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start(); //On démarre une session
-
-    session_destroy(); //On détruit une session
-    header('Location:index.php'); //On redirige vers index.php
-    exit;
-    ?>
